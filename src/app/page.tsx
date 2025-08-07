e1909878ddb88f3e706813758fdcb3ed3b5637c3
W!LL <kimaniwilfred95@gmail.com>
2025-08-07 20:16:49 +0300

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
     const user = localStorage.getItem("user");
-    if (user)
-        redirect("/dashboard");
-    else
-        redirect("/login");
-    return null;
+    if (user) {
+      router.replace("/dashboard");
+    } else {
+      router.replace("/login");
+    }
+  }, [router]);
+
+  return null;
 }
